97e76cd10a3879997f73a448d0ad90cd409a4309
Przemek738 <przemyslaw.pyrzynski@studenci.collegiumwitelona.pl>
2026-05-18 14:50:41 +0200

    Create Tables

---
+++ Models/Platform.cs
@@ -0,0 +1,10 @@
+﻿namespace BacklogManager.Models;
+
+public class Platform
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    
+    public List<UserGame> UserGames { get; set; } = new();
+}
---
+++ Models/UserGame.cs
@@ -0,0 +1,17 @@
+﻿namespace BacklogManager.Models;
+
+public class UserGame
+{
+    public int Id { get; set; }
+    
+    public int UserId { get; set; }
+    public User User { get; set; }
+
+    public int GameId { get; set; }
+    public Game Game { get; set; }
+    public List<Platform> Platforms { get; set; } = new();
+    
+    public string Status { get; set; }
+    public int? Rating { get; set; }
+    public string? Notes { get; set; }
+}
