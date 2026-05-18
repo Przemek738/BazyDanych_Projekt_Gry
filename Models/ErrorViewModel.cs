8b253a91eea0835521d0184e221b1d99e7417f44
Przemek738 <przemyslaw.pyrzynski@studenci.collegiumwitelona.pl>
2026-05-18 11:58:10 +0200

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BacklogManager.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
