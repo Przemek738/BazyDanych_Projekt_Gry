89cf45d17393a4728dead15362689c24a062a880
MichalNapora <michal.napora@studenci.collegiumwitelona.pl>
2026-05-18 14:35:30 +0200

    Class_Game

---
+++ Models/Game.cs
@@ -0,0 +1,11 @@
+﻿namespace BacklogManager.Models;
+
+public class Game
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Developer { get; set; }
+    public int ReleaseYear { get; set; }
+    public string Genre { get; set; }
+    public string ImageUrl { get; set; } 
+}
