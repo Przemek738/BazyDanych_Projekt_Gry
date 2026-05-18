436a2d0b84bba30c338bf9851850dc24c5b5228e
Przemek738 <przemyslaw.pyrzynski@studenci.collegiumwitelona.pl>
2026-05-18 15:23:59 +0200

    Adding DTO

---
+++ DTOs/GameEditViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BacklogManager.DTOs;
+public class GameEditViewModel
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Developer { get; set; }
+    public int ReleaseYear { get; set; }
+    public string Genre { get; set; }
+    public string ImageUrl { get; set; }
+}
